897ab2a03cbde0be4cdbeb2fd71cedfca42545ea
nakumar <nakumar@fanatics.com>
2023-07-24 21:28:47 +0530

    day5

---
+++ ScalaProgramming/src/main/scala/Day5/WhileLoops.scala
@@ -0,0 +1,24 @@
+package Day5
+
+import scala.collection.mutable.ArrayBuffer
+import scala.io.StdIn
+
+object WhileLoops {
+  def main(args: Array[String]): Unit = {
+
+    val usernames = ArrayBuffer("Naveen", "Kumar", "bhavani")
+    var continueFlag = true
+
+    while (continueFlag) {
+      println("Please enter the username")
+      val uname = StdIn.readLine()
+      if (!usernames.contains(uname)) {
+        continueFlag = false
+        usernames.addOne(uname)
+      }else{
+        println("user name already exists")
+      }
+      println(s"usernames are $usernames")
+    }
+  }
+}
