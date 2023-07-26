5fab00c7076b394b02d45ce7d3aced5c3980bef4
nakumar <nakumar@fanatics.com>
2023-07-26 07:40:41 +0530

    day6

---
+++ ScalaProgramming/src/main/scala/Day6/Function2.scala
@@ -0,0 +1,33 @@
+package Day6
+
+import scala.io.StdIn
+
+object Function2 {
+  def main(args: Array[String]): Unit = {
+   val products:Map[String,Double] = Map("Shoes" -> 10, "Wallet" -> 15.1, "t-shirt" -> 23.1)
+
+    def addTax(name:String) ={
+      val price = products.get(name)
+      if (price.nonEmpty) {
+        price.get.*(0.20) + price.get
+      }else{
+        0.0
+      }
+    }
+
+    println(s"calculating the tax for product shoes ${addTax("Shoes")}")
+    println(s"calcucating the tax for the product watch ${addTax("watch")}")
+
+    val animalsLifeSpan:Map[String,Int] = Map("cats" -> 15,"dogs" -> 10, "rabbit" -> 12)
+
+    println("Please provide the animal age to know the life span")
+    val userAnimal = StdIn.readLine().toString
+
+    def AnimalLifeSpan(userAnimal:String)={
+      if (animalsLifeSpan.contains(userAnimal)) animalsLifeSpan(userAnimal) else 20
+    }
+
+    println(s"requested lifespan for $userAnimal is ${AnimalLifeSpan(userAnimal)}")
+
+  }
+}
---
+++ ScalaProgramming/src/main/scala/Day6/Functions.scala
@@ -0,0 +1,31 @@
+package Day6
+
+import scala.io.StdIn
+
+object Functions {
+  def main(args: Array[String]): Unit = {
+    println("please enter the name:")
+    val username = StdIn.readLine()
+    println("please enter your age:")
+    val age = StdIn.readLine().toInt
+
+    def hello(username: String, age: Int) = println(s"I am $username and i am $age years old.")
+
+    hello(username, age)
+
+    //Functions accept the parameters of any data type as mentioned below.
+    def double(a: Int, b: Int = 5, c: Int) = a + b + c
+
+    /* it will error out, if we pass less number of arguments to a function
+    function will be invoked in the sequence of the parameters defined.
+    if we want to make use of a default parameters then we will have to call using the named agruments
+     */
+
+    //error out, expecting the b to use the default value
+    //double(1,2)
+
+    // sucessful invokation
+    double(a = 1, c = 10)
+
+  }
+}
